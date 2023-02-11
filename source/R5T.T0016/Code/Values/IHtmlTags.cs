c169c9aaae511d75b07db2a8cd55d5f440206ad0
davidcoats <david@getsafetycone.com>
2023-02-10 17:50:54 -0800

    Interim changes.

--- source/R5T.T0016/Code/Values/IHtmlTags.cs
+++ source/R5T.T0016/Code/Values/IHtmlTags.cs
@@ -8,13 +8,16 @@ namespace R5T.T0016
     [ValuesMarker]
     public partial interface IHtmlTags : IValuesMarker
     {
-        public const string button = "button";
-        public const string Button = IHtmlTags.button;
-        public const string div = "div";
-        public const string Divider = IHtmlTags.div;
-        public const string img = "img";
-        public const string Image = IHtmlTags.img;
-        public const string span = "span";
-        public const string Span = IHtmlTags.span;
+#pragma warning disable IDE1006 // Naming Styles
+        public string button => "button";
+        public string div => "div";
+        public string img => "img";
+        public string span => "span";
+#pragma warning restore IDE1006 // Naming Styles
+
+        public string Button => this.button;
+        public string Divider => this.div;
+        public string Image => this.img;
+        public string Span => this.span;
     }
 }

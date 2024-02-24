6977a2b36fffaf72599ff66361e159b3123159e3
David Coats <david@getsafetycone.com>
2024-02-24 00:53:59 -0800

    Update method name.

--- source/R5T.T0016/Code/Functionality/IHtmlTextGenerator.cs
+++ source/R5T.T0016/Code/Functionality/IHtmlTextGenerator.cs
@@ -10,7 +10,7 @@ namespace R5T.T0016
     {
         public string GetCopyrightText(string copyrightHolderName)
         {
-            var currentYear = Instances.NowOperator.GetCurrentYear();
+            var currentYear = Instances.NowOperator.Get_CurrentYear();
 
             var output = $"&copy; {currentYear} {copyrightHolderName}. All rights reserved.";
             return output;
